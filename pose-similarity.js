5a4d12553223a41e7f356f969acc401d43b88cc6
steven9408 <whdufo@gmail.com>
2021-09-17 03:21:57 +0900

    fix: weigtedDastance edit

--- pose-similarity.js
+++ pose-similarity.js
@@ -6,207 +6,204 @@ module.exports = function(RED) {
             const similarity = require("compute-cosine-similarity"); 
             let keypoints = JSON.parse(msg.payload)
             
-            // console.log(keypoints)
-            // 
-            // console.log(keypoinstsPreprocessing(keypoints))
-            let sample = [
+            sample = [
                 {
-                    x: 0.4817624092102051,
-                    y: 0.45846912264823914,
-                    z: -0.7698102593421936,
-                    visibility: 0.9998458027839661
+                    x: 0.5747062563896179,
+                    y: 0.4066506624221802,
+                    z: -0.9112027287483215,
+                    visibility: 0.9999215602874756
                 },
                 {
-                    x: 0.4936062693595886,
-                    y: 0.40500861406326294,
-                    z: -0.7621992230415344,
-                    visibility: 0.9996991753578186
+                    x: 0.5866062641143799,
+                    y: 0.3473609387874603,
+                    z: -0.898028552532196,
+                    visibility: 0.9998391270637512
                 },
                 {
-                    x: 0.502403974533081,
-                    y: 0.4040256440639496,
-                    z: -0.7621386647224426,
-                    visibility: 0.9996520280838013
+                    x: 0.5959856510162354,
+                    y: 0.3474617600440979,
+                    z: -0.8972940444946289,
+                    visibility: 0.9997789263725281
                 },
                 {
-                    x: 0.5104445219039917,
-                    y: 0.4026203453540802,
-                    z: -0.7621181607246399,
-                    visibility: 0.9996649026870728
+                    x: 0.6042131185531616,
+                    y: 0.3469957709312439,
+                    z: -0.8980273008346558,
+                    visibility: 0.9997367262840271
                 },
                 {
-                    x: 0.4656929075717926,
-                    y: 0.4021480083465576,
-                    z: -0.7685867547988892,
-                    visibility: 0.9997730851173401
+                    x: 0.5535554885864258,
+                    y: 0.34255245327949524,
+                    z: -0.9140981435775757,
+                    visibility: 0.999924898147583
                 },
                 {
-                    x: 0.4542573392391205,
-                    y: 0.3997102379798889,
-                    z: -0.768586277961731,
-                    visibility: 0.9997762441635132
+                    x: 0.5404933094978333,
+                    y: 0.3399371802806854,
+                    z: -0.9140849113464355,
+                    visibility: 0.9999299645423889
                 },
                 {
-                    x: 0.4439796507358551,
-                    y: 0.39816251397132874,
-                    z: -0.7685798406600952,
-                    visibility: 0.9998146295547485
+                    x: 0.5277708768844604,
+                    y: 0.3379674553871155,
+                    z: -0.9140845537185669,
+                    visibility: 0.9999411106109619
                 },
                 {
-                    x: 0.5196115970611572,
-                    y: 0.4010966718196869,
-                    z: -0.5862746834754944,
-                    visibility: 0.9995452165603638
+                    x: 0.6041008234024048,
+                    y: 0.3492162525653839,
+                    z: -0.6894615888595581,
+                    visibility: 0.9997290372848511
                 },
                 {
-                    x: 0.42232346534729004,
-                    y: 0.39792299270629883,
-                    z: -0.6093929409980774,
-                    visibility: 0.9998736381530762
+                    x: 0.4962088167667389,
+                    y: 0.34016531705856323,
+                    z: -0.7658218741416931,
+                    visibility: 0.9999710321426392
                 },
                 {
-                    x: 0.4940943717956543,
-                    y: 0.49753955006599426,
-                    z: -0.6936413049697876,
-                    visibility: 0.9999092221260071
+                    x: 0.582728385925293,
+                    y: 0.45060861110687256,
+                    z: -0.8215945363044739,
+                    visibility: 0.9999464750289917
                 },
                 {
-                    x: 0.4609544277191162,
-                    y: 0.5041597485542297,
-                    z: -0.6992852687835693,
-                    visibility: 0.9999619722366333
+                    x: 0.5492018461227417,
+                    y: 0.45132675766944885,
+                    z: -0.842786967754364,
+                    visibility: 0.999986469745636
                 },
                 {
-                    x: 0.5845454931259155,
-                    y: 0.615190863609314,
-                    z: -0.4180196225643158,
-                    visibility: 0.9996994137763977
+                    x: 0.6481769680976868,
+                    y: 0.5653575658798218,
+                    z: -0.4546165466308594,
+                    visibility: 0.9997624158859253
                 },
                 {
-                    x: 0.3380344808101654,
-                    y: 0.6093859672546387,
-                    z: -0.47039955854415894,
-                    visibility: 0.9998713135719299
+                    x: 0.3770679831504822,
+                    y: 0.5576112866401672,
+                    z: -0.5854411125183105,
+                    visibility: 0.999977171421051
                 },
                 {
-                    x: 0.6179484724998474,
-                    y: 0.9256265163421631,
-                    z: -0.31697335839271545,
-                    visibility: 0.8543384671211243
+                    x: 0.6429665088653564,
+                    y: 0.7942731976509094,
+                    z: -0.06745405495166779,
+                    visibility: 0.8866057395935059
                 },
                 {
-                    x: 0.282159686088562,
-                    y: 0.9437262415885925,
-                    z: -0.3802521824836731,
-                    visibility: 0.8950935006141663
+                    x: 0.2910415530204773,
+                    y: 0.8575465679168701,
+                    z: -0.4180843234062195,
+                    visibility: 0.9845975041389465
                 },
                 {
-                    x: 0.6231604814529419,
-                    y: 1.1875196695327759,
-                    z: -0.380183607339859,
-                    visibility: 0.28457868099212646
+                    x: 0.6301985383033752,
+                    y: 1.0686594247817993,
+                    z: 0.12654970586299896,
+                    visibility: 0.35036349296569824
                 },
                 {
-                    x: 0.23462557792663574,
-                    y: 1.219119668006897,
-                    z: -0.5022183656692505,
-                    visibility: 0.42301344871520996
+                    x: 0.2632238566875458,
+                    y: 1.1936697959899902,
+                    z: -0.41257113218307495,
+                    visibility: 0.6956201791763306
                 },
                 {
-                    x: 0.6372942328453064,
-                    y: 1.2619297504425049,
-                    z: -0.43001788854599,
-                    visibility: 0.21158675849437714
+                    x: 0.6401009559631348,
+                    y: 1.1512751579284668,
+                    z: 0.15113599598407745,
+                    visibility: 0.26908233761787415
                 },
                 {
-                    x: 0.20873555541038513,
-                    y: 1.2932957410812378,
-                    z: -0.5520216822624207,
-                    visibility: 0.31271880865097046
+                    x: 0.24309027194976807,
+                    y: 1.2793532609939575,
+                    z: -0.42840415239334106,
+                    visibility: 0.5567180514335632
                 },
                 {
-                    x: 0.6163712739944458,
-                    y: 1.2666116952896118,
-                    z: -0.4700315594673157,
-                    visibility: 0.2814890444278717
+                    x: 0.6212316155433655,
+                    y: 1.1468985080718994,
+                    z: 0.11414127051830292,
+                    visibility: 0.3027908205986023
                 },
                 {
-                    x: 0.23067955672740936,
-                    y: 1.2925909757614136,
-                    z: -0.6063997149467468,
-                    visibility: 0.3960041105747223
+                    x: 0.27280905842781067,
+                    y: 1.279313325881958,
+                    z: -0.47806820273399353,
+                    visibility: 0.6154868602752686
                 },
                 {
-                    x: 0.6036989092826843,
-                    y: 1.2410392761230469,
-                    z: -0.4043017029762268,
-                    visibility: 0.2950258255004883
+                    x: 0.6136063933372498,
+                    y: 1.1363861560821533,
+                    z: 0.1184360533952713,
+                    visibility: 0.3139689266681671
                 },
                 {
-                    x: 0.2480548918247223,
-                    y: 1.26681387424469,
-                    z: -0.5298413038253784,
-                    visibility: 0.4079611301422119
+                    x: 0.28665536642074585,
+                    y: 1.2404080629348755,
+                    z: -0.42932090163230896,
+                    visibility: 0.6263441443443298
                 },
                 {
-                    x: 0.529731273651123,
-                    y: 1.1422696113586426,
-                    z: -0.006370258517563343,
-                    visibility: 0.029704930260777473
+                    x: 0.5563026666641235,
+                    y: 1.0931206941604614,
+                    z: 0.018255433067679405,
+                    visibility: 0.16300508379936218
                 },
                 {
-                    x: 0.37706810235977173,
-                    y: 1.1414549350738525,
-                    z: 0.007632910273969173,
-                    visibility: 0.031471796333789825
+                    x: 0.4019245505332947,
+                    y: 1.115423321723938,
+                    z: -0.017303040251135826,
+                    visibility: 0.20045308768749237
                 },
                 {
-                    x: 0.5251245498657227,
-                    y: 1.59298574924469,
-                    z: 0.1452331840991974,
-                    visibility: 0.008981773629784584
+                    x: 0.5289310216903687,
+                    y: 1.6041017770767212,
+                    z: 0.20637676119804382,
+                    visibility: 0.016674384474754333
                 },
                 {
-                    x: 0.3849889636039734,
-                    y: 1.5978494882583618,
-                    z: 0.1252935230731964,
-                    visibility: 0.004145687911659479
+                    x: 0.3944813013076782,
+                    y: 1.5989164113998413,
+                    z: 0.08016983419656754,
+                    visibility: 0.011937452480196953
                 },
                 {
-                    x: 0.5217388868331909,
-                    y: 1.983454942703247,
-                    z: 0.5554765462875366,
-                    visibility: 0.0010348185896873474
+                    x: 0.5030412077903748,
+                    y: 2.0019679069519043,
+                    z: 0.6629712581634521,
+                    visibility: 0.0015568474773317575
                 },
                 {
-                    x: 0.3917146325111389,
-                    y: 1.986128568649292,
-                    z: 0.429389625787735,
-                    visibility: 0.00029201325378380716
+                    x: 0.3640366196632385,
+                    y: 2.0039117336273193,
+                    z: 0.408647358417511,
+                    visibility: 0.0011314389994367957
                 },
                 {
-                    x: 0.5217303037643433,
-                    y: 2.0401320457458496,
-                    z: 0.5843878984451294,
-                    visibility: 0.001181385014206171
+                    x: 0.4931778013706207,
+                    y: 2.0565028190612793,
+                    z: 0.6943466067314148,
+                    visibility: 0.0011479948880150914
                 },
                 {
-                    x: 0.3905019164085388,
-                    y: 2.042186737060547,
-                    z: 0.4530583322048187,
-                    visibility: 0.0003971754922531545
+                    x: 0.3537369966506958,
+                    y: 2.0598084926605225,
+                    z: 0.4315360188484192,
+                    visibility: 0.001483823056332767
                 },
                 {
-                    x: 0.5053785443305969,
-                    y: 2.1258273124694824,
-                    z: 0.32744300365448,
-                    visibility: 0.0011948851170018315
+                    x: 0.4993167817592621,
+                    y: 2.1468515396118164,
+                    z: 0.4452536106109619,
+                    visibility: 0.0037718492094427347
                 },
                 {
-                    x: 0.4157027304172516,
-                    y: 2.1308703422546387,
-                    z: 0.13514506816864014,
-                    visibility: 0.0004240306734573096
+                    x: 0.383365660905838,
+                    y: 2.1454927921295166,
+                    z: 0.10924339294433594,
+                    visibility: 0.0016091909492388368
                 }
             ]
             
@@ -236,12 +233,12 @@ module.exports = function(RED) {
                     ...sampleKeypoints.xTerms,
                     ...sampleKeypoints.yTerms,
                     ...sampleKeypoints.zTerms,
-                    ...sampleKeypoints.scores
                 ],
-                sampleKeypoints.xTerms.length
+                nowKeypoints.xTerms.length
             )
-            console.log((output1+output2)/2)
+            
 
+            console.log(output1, output2)
             function keypoinstsPreprocessing(keypoints) {
                 // Pre-processing : Nomalization and Making Vector
                 let xTerms = []
@@ -255,12 +252,22 @@ module.exports = function(RED) {
                     let z = keypoints[i].z
                     let s = keypoints[i].visibility
                     
-                    l2norm = Math.sqrt(x**2 + y**2 + z**2)
-                    xTerms.push(x/l2norm)
-                    yTerms.push(y/l2norm)
-                    zTerms.push(z/l2norm)
+                    l2norm = x**2 + y**2 + z**2
+                    
+                    xTerms.push(x)
+                    yTerms.push(y)
+                    zTerms.push(z)
                     scores.push(s)  
                 }
+
+                l2norm = Math.sqrt(l2norm)
+
+                for ( let i = 0; i < keypoints.length; i++) {
+                    xTerms[i] /= l2norm
+                    yTerms[i] /= l2norm
+                    zTerms[i] /= l2norm
+                }
+                
 
                 return {xTerms, yTerms, zTerms, scores}
 
@@ -268,25 +275,30 @@ module.exports = function(RED) {
 
             function cosineDistanceMatching(poseVector1, poseVector2) {
                 // Cosine Similarity matching
-                let cosineSimilarity = similarity(poseVector1, poseVector2);
-                let distance = 2 * (1 - cosineSimilarity);
+                let cosineSimilarity = similarity(poseVector1, poseVector2)
+                let distance = 2 * (1 - cosineSimilarity)
                 return Math.sqrt(distance);
             }
 
             function weightedDistanceMatching(poseVector1, poseVector2, keypointsNum) {
                 // Weighted Matching using score 
-                let vector1PoseXY = poseVector1.slice(0, keypointsNum*2);
-                let vector1Confidences = poseVector1.slice(keypointsNum*2, poseVector1.length);
-                let vector2PoseXY = poseVector2.slice(0, keypointsNum*2);
-                let summation1 = 0;
-                let summation2 = 0;
-                for (let i = 0; i < vector1PoseXY.length; i++) {
-                    let tempConf = Math.floor(i / 3);
-                    let tempSum = vector1Confidences[tempConf] * Math.abs(vector1PoseXY[i] - vector2PoseXY[i]);
-                    summation1 = summation1 + vector1Confidences[tempConf];
-                    summation2 = summation2 + tempSum;
+                // poseVector1 : 인풋 포즈
+                // poseVector2 : 저장된 포즈 
+                let vector1PoseXY = poseVector1.slice(0, keypointsNum*3)
+                let vector1Confidences = poseVector1.slice(keypointsNum*3, poseVector1.length)
+                let vector2PoseXY = poseVector2.slice(0, keypointsNum*3)
+                let summation1 = 0
+                let summation2 = 0
+                for (let i = 0; i < vector1Confidences.length; i++) {
+                    let tempSum = 0
+                    tempSum += Math.abs(vector1PoseXY[i] - vector2PoseXY[i])
+                    tempSum += Math.abs(vector1PoseXY[keypointsNum + i] - vector2PoseXY[keypointsNum + i])
+                    tempSum += Math.abs(vector1PoseXY[keypointsNum*2 + i] - vector2PoseXY[keypointsNum*2 + i])
+
+                    summation1 += vector1Confidences[i]
+                    summation2 += tempSum * vector1Confidences[i]
                 }
-                return 3 * summation2 / summation1;
+                return summation2 / summation1;
             }
             msg.payload = "Hi";
             node.send(msg);
