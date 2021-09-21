f5ddfb176d047e137f454e02840835e7a4e0d9f5
윤이진 <483759@naver.com>
2021-09-21 18:57:21 +0900

    add: motion registration timer setting

--- detection/detection.js
+++ detection/detection.js
@@ -10,183 +10,193 @@ module.exports = function(RED) {
 
         function HTML() {
           const html = String.raw`
+
           <!DOCTYPE html>
-          <html>
-          
-          <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/@mediapipe/control_utils/control_utils.js" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/@mediapipe/control_utils_3d/control_utils_3d.js"
-              crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils/drawing_utils.js" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/@mediapipe/pose/pose.js" crossorigin="anonymous"></script>
-            <title>Pose Detection</title>
-            <style>
-              .tooltip {
-                position: relative;
-                display: inline-block;
-              }
-          
-              .tooltip .tooltip-content {
-                visibility: hidden;
-                background-color: rgba(255, 255, 255, 0.8);
-                color: black;
-                text-align: center;
-                position: absolute;
-                top: 3px;
-                left: 3px;
-                padding-left: 15px;
-                padding-right: 15px;
-                margin-top: 0px;
-                border-radius: 10px;
-                z-index: 1;
-              }
-          
-              .tooltip:hover .tooltip-content {
-                visibility: visible;
-              }
-          
-              #regist-btn {
-                background-color: #B2A1F4;
-                border: 1px solid grey;
-                border-left: none;
-                height: 21px;
-                color: white;
-              }
-          
-              #regist-btn:hover {
-                background-color: #7557f0;
-                cursor: pointer;
-              }
-            </style>
-          </head>
-          
-          <body>            
-            <div align="center" style="min-height: 800px;">
-              <h1>Pose Detection Page</h1>
-              <div style="display: inline-block;" align="center" class="tooltip">
-                <video class="input_video" width="600px" height="340px" crossorigin="anonymous" style="border:3px solid grey"></video><br>
-                <div class="tooltip-content">
-                  <p>Your Camera</p>
-                </div>
-              </div>
-              <div style="display: inline-block;" align="center" class="tooltip">
-                <canvas class="output_canvas" width="600px" height="340px" style="border:3px solid #B2A1F4"></canvas><br>
-                <div class="tooltip-content">
-                  <p>Tracking your Pose</p>
-                </div>
-              </div>
-              <div>
-                <br>
-                <select id="secondTimer">
-                  <option value="0" selected>Now</option>
-                  <option value="1">1s Timer</option>
-                  <option value="2">2s Timer</option>
-                  <option value="3">3s Timer</option>
-                </select> 
-                <input id="pose-motion-name"type="text" placeholder="Hands Motion Name"><button id="regist-btn">Regist</button>
-              </div>
-              <div>
-                <p id="motion-result-message"></p>
-              </div>
-            </div>  
-            <hr>
-            <div align="center">
-              <a href="https://github.com/5FNSaaS">5FNSaaS</a>
-            </div>
-          </body>
-          
-          </html>
-          
-          <script type="module">
-            /* motion regist timer */
-            const timerSecond = document.getElementById("secondTimer");
-            var second = timerSecond.options[timerSecond.selectedIndex].value;
-            console.log(second);
-          
-            document.getElementById("secondTimer").addEventListener('change', function(){
-              second = timerSecond.options[timerSecond.selectedIndex].value;
-              console.log(second);
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@mediapipe/control_utils/control_utils.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@mediapipe/control_utils_3d/control_utils_3d.js"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils/drawing_utils.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@mediapipe/pose/pose.js" crossorigin="anonymous"></script>
+  <title>Pose Detection</title>
+  <style>
+    .tooltip {
+      position: relative;
+      display: inline-block;
+    }
+
+    .tooltip .tooltip-content {
+      visibility: hidden;
+      background-color: rgba(255, 255, 255, 0.8);
+      color: black;
+      text-align: center;
+      position: absolute;
+      top: 3px;
+      left: 3px;
+      padding-left: 15px;
+      padding-right: 15px;
+      margin-top: 0px;
+      border-radius: 10px;
+      z-index: 1;
+    }
+
+    .tooltip:hover .tooltip-content {
+      visibility: visible;
+    }
+
+    #regist-btn {
+      background-color: #B2A1F4;
+      border: 1px solid grey;
+      border-left: none;
+      height: 21px;
+      color: white;
+    }
+
+    #regist-btn:hover {
+      background-color: #7557f0;
+      cursor: pointer;
+    }
+  </style>
+</head>
+
+<body>            
+  <div align="center" style="min-height: 800px;">
+    <h1>Pose Detection Page</h1>
+    <div style="display: inline-block;" align="center" class="tooltip">
+      <video class="input_video" width="600px" height="340px" crossorigin="anonymous" style="border:3px solid grey"></video><br>
+      <div class="tooltip-content">
+        <p>Your Camera</p>
+      </div>
+    </div>
+    <div style="display: inline-block;" align="center" class="tooltip">
+      <canvas class="output_canvas" width="600px" height="340px" style="border:3px solid #B2A1F4"></canvas><br>
+      <div class="tooltip-content">
+        <p>Tracking your Pose</p>
+      </div>
+    </div>
+    <div>
+      <br>
+      <select id="secondTimer">
+        <option value="0" selected>Now</option>
+        <option value="1">1s Timer</option>
+        <option value="2">2s Timer</option>
+        <option value="3">3s Timer</option>
+      </select> 
+      <input id="pose-motion-name" type="text" placeholder="Motion Pose Name"><button id="regist-btn">Regist</button>
+    </div>
+    <div>
+      <p id="motion-result-message"></p>
+    </div>
+  </div>  
+  <hr>
+  <div align="center">
+    <a href="https://github.com/5FNSaaS">5FNSaaS</a>
+  </div>
+</body>
+
+</html>
+
+<script type="module">
+  /* motion regist timer */
+  const timerSecond = document.getElementById("secondTimer");
+  var second = timerSecond.options[timerSecond.selectedIndex].value;
+  console.log(second);
+
+  document.getElementById("secondTimer").addEventListener('change', function(){
+    second = timerSecond.options[timerSecond.selectedIndex].value;
+    console.log(second);
+  })
+
+  /* motion name empty check*/
+  var poseMotionName = document.getElementById("pose-motion-name");
+  var curLandmarks = null
+  document.getElementById("regist-btn").addEventListener('click', function(){
+    if(poseMotionName.value === "" || poseMotionName.value === undefined){
+      document.getElementById("motion-result-message").textContent = "[Fail] Invalid Motion-Name";
+    }else{
+      const poseName = poseMotionName.value;
+      
+      setTimeout((poseName)=>{
+          console.log(poseName);
+            var result = $.post("regist", {"name": poseName, "keypoints": curLandmarks}, function(data){
+              alert("success");
+            }).done(function(){
+              alert("second success");
+            }).fail(function(){
+              alert("error");
             })
+            //console.log(poseName);
+            //console.log(document.getElementById("secondTimer").value*1000);
+            document.getElementById("motion-result-message").textContent = "Regist Success! You can used [" + poseName +"] motion";
           
-            /* motion name empty check*/
-            var handMotionName = document.getElementById("pose-motion-name");
-            var curLandmarks = null
-            document.getElementById("regist-btn").addEventListener('click', function(){
-              if(handMotionName.value === "" || handMotionName.value === undefined){
-                document.getElementById("motion-result-message").textContent = "[Fail] Invalid Motion-Name";
-              }else{
-                var result = $.post("regist", {"name": handMotionName.value, "keypoints": curLandmarks}, function(data){
-                  alert("success");
-                }).done(function(){
-                  alert("second success");
-                }).fail(function(){
-                  alert("error");
-                })
-                document.getElementById("motion-result-message").textContent = "Regist Success! You can used [" + handMotionName.value +"] motion";
-              }
-          
-              document.getElementById("pose-motion-name").value = "";
-            })
-          
-            const videoElement = document.getElementsByClassName('input_video')[0]
-            const canvasElement = document.getElementsByClassName('output_canvas')[0]
-            const canvasCtx = canvasElement.getContext('2d')
-          
-            const ws = new WebSocket('ws://localhost:1880/ws/mediapipe')
-          
-            function onResults(results) {
-          
-              canvasCtx.save()
-              canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height)
-              canvasCtx.globalCompositeOperation = 'destination-atop'
-              canvasCtx.drawImage(
-                results.image, 0, 0, canvasElement.width, canvasElement.height)
-          
-              canvasCtx.globalCompositeOperation = 'source-over'
-              drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
-                { color: '#00FF00', lineWidth: 4 })
-              drawLandmarks(canvasCtx, results.poseLandmarks,
-                { color: '#FF0000', lineWidth: 2 })
-              canvasCtx.restore()
-          
-              if (results.poseLandmarks) {
-                curLandmarks = results.poseLandmarks
-                ws.send(JSON.stringify(results.poseLandmarks))
-              }
-            }
-          
-            const pose = new Pose({
-              locateFile: (file) => {
-                return 'https://cdn.jsdelivr.net/npm/@mediapipe/pose/' + file
-              }
-            })
-          
-            pose.setOptions({
-              modelComplexity: 1,
-              smoothLandmarks: true,
-              enableSegmentation: true,
-              smoothSegmentation: true,
-              minDetectionConfidence: 0.5,
-              minTrackingConfidence: 0.5
-            })
-          
-            pose.onResults(onResults)
-          
-            const camera = new Camera(videoElement, {
-              onFrame: async () => {
-                await pose.send({ image: videoElement })
-              },
-              width: 1280,
-              height: 720
-            })
-            camera.start()
-          </script>
-          
+        }, document.getElementById("secondTimer").value*1000, poseName);
+    }
+
+    document.getElementById("pose-motion-name").value = "";
+  })
+
+  const videoElement = document.getElementsByClassName('input_video')[0]
+  const canvasElement = document.getElementsByClassName('output_canvas')[0]
+  const canvasCtx = canvasElement.getContext('2d')
+
+  const ws = new WebSocket('ws://localhost:1880/ws/mediapipe')
+
+  function onResults(results) {
+
+    canvasCtx.save()
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height)
+    canvasCtx.globalCompositeOperation = 'destination-atop'
+    canvasCtx.drawImage(
+      results.image, 0, 0, canvasElement.width, canvasElement.height)
+
+    canvasCtx.globalCompositeOperation = 'source-over'
+    drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+      { color: '#00FF00', lineWidth: 4 })
+    drawLandmarks(canvasCtx, results.poseLandmarks,
+      { color: '#FF0000', lineWidth: 2 })
+    canvasCtx.restore()
+
+        if (results.poseLandmarks) {
+          curLandmarks = results.poseLandmarks
+          ws.send(JSON.stringify(results.poseLandmarks))
+        }
+  }
+
+  const pose = new Pose({
+    locateFile: (file) => {
+      return 'https://cdn.jsdelivr.net/npm/@mediapipe/pose/' + file
+    }
+  })
+
+  pose.setOptions({
+    modelComplexity: 1,
+    smoothLandmarks: true,
+    enableSegmentation: true,
+    smoothSegmentation: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+  })
+
+  pose.onResults(onResults)
+
+  const camera = new Camera(videoElement, {
+    onFrame: async () => {
+      await pose.send({ image: videoElement })
+    },
+    width: 1280,
+    height: 720
+  })
+  camera.start()
+</script>
+
+
           
           `
           return html
