6644964e5e8f2e40020b5b247a934e16d32cf801
Changhwan Son <48404869+Changhwan-Son@users.noreply.github.com>
2021-10-05 16:12:06 +0900

    [feat] add xml filtering in shopping-filter node

--- Meoseon/shopping-filter.js
+++ Meoseon/shopping-filter.js
@@ -1,10 +1,37 @@
 module.exports = function (RED) {
+  const parser = require('fast-xml-parser');
+  const he = require('he');
+
   function ShoppingFilter(config) {
     RED.nodes.createNode(this, config);
     var node = this;
 
     node.on('input', function (msg) {
       if (config.apiType === 'naver') {
+        if (config.format === 'xml') {
+          var options = {
+            attributeNamePrefix: '@_',
+            attrNodeName: 'attr', //default is 'false'
+            textNodeName: '#text',
+            ignoreAttributes: true,
+            ignoreNameSpace: false,
+            allowBooleanAttributes: false,
+            parseNodeValue: true,
+            parseAttributeValue: false,
+            trimValues: true,
+            cdataTagName: '__cdata', //default is 'false'
+            cdataPositionChar: '\\c',
+            parseTrueNumberOnly: false,
+            arrayMode: false, //"strict"
+            attrValueProcessor: (val, attrName) => he.decode(val, { isAttributeValue: true }), //default is a=>a
+            tagValueProcessor: (val, tagName) => he.decode(val), //default is a=>a
+            stopNodes: ['parse-me-as-string'],
+          };
+
+          var jsonObj = parser.parse(msg.payload, options);
+          msg.payload = jsonObj;
+          msg.payload.items = msg.payload.rss.channel.item;
+        }
         var list = msg.payload.items;
 
         if (config.minprice)
