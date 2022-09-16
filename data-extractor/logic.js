b387e81738d5c8f36e87b4f76f8ca7c6761716c5
박승훈 <tmdgns1126@naver.com>
2022-09-16 14:13:41 +0900

    feat: create business logic of extractor and filter node

---
+++ data-extractor/logic.js
@@ -0,0 +1,369 @@
+const json = `{
+  "response": {
+    "header": {
+      "resultCode": "00",
+      "resultMsg": "NORMAL_CODE"
+    },
+    "body": {
+      "items": {
+        "item": [
+          {
+            "RNUM": 1.0,
+            "SPECIES_KEY": "72044138",
+            "BOOK_KEY": "72044140",
+            "IDX_TITLE": "클로즈업도쿄 클로즈업 도쿄 CLOSEUPTOKYO CLOSE UPTOKYO UP TOKYO 도쿄디즈니리조트요코하마하코네닛코카마쿠라 디즈니리조트요코하마하코네닛코카마쿠라 디즈니 리조트요코하마하코네닛코카마쿠라 리조트 요코하마하코네닛코카마쿠라 요코하마 하코네닛코카마쿠라 하코네 닛코카마쿠라 닛코 카마쿠라",
+            "IDX_AUTHOR": "유재우 손미경 유재우손미경지음",
+            "IDX_PUBLISHER": "에디터",
+            "IDX_KEYWORD": "클로즈업 도쿄 일본 일본여행 도쿄여행 TOKYO",
+            "TITLE_INFO": "클로즈업 도쿄 = Close up Tokyo : 도쿄 디즈니 리조트, 요코하마, 하코네, 닛코, 카마쿠라",
+            "AUTHOR_INFO": "유재우, 손미경 지음.",
+            "PUBLISHER": "에디터",
+            "PUB_YEAR_INFO": "2015.",
+            "MANAGE_CODE": "MB",
+            "MANAGE_DESC": "도봉어린이문화정보도서관",
+            "SUBJECT_CODE": "9",
+            "SUBJECT_DESC": "역사",
+            "USE_LIMIT_CODE": "GM",
+            "USE_LIMIT_DESC": "일반",
+            "ISBN": "9788967440886",
+            "REG_NO": "AR0000013107",
+            "SHELF_LOC_CODE": "001",
+            "SHELF_LOC_DESC": "일반자료실",
+            "SEPARATE_SHELF_CODE": "",
+            "SEPARATE_SHELF_DESC": "적용안함",
+            "CLASS_NO": "981.302",
+            "BOOK_CODE": "유72ㄷ",
+            "WORKING_STATUS": "BOL112N",
+            "WORKING_STATUS_DESC": "배가자료(소장자료)",
+            "BOOK_APPENDIX_FLAG": "B",
+            "BOOK_APPENDIX_DESC": "책"
+          },
+          {
+            "RNUM": 2.0,
+            "SPECIES_KEY": "72044142",
+            "BOOK_KEY": "72044144",
+            "IDX_TITLE": "공부의달인호모쿵푸스 공부의 달인호모쿵푸스 달인 호모쿵푸스 호모 쿵푸스 달인시리즈 시리즈",
+            "IDX_AUTHOR": "고미숙 고미숙지음",
+            "IDX_PUBLISHER": "북드라망",
+            "IDX_KEYWORD": "공부 달인 호모쿵푸스",
+            "TITLE_INFO": "(공부의 달인) 호모 쿵푸스",
+            "AUTHOR_INFO": "고미숙 지음.",
+            "PUBLISHER": "북드라망",
+            "PUB_YEAR_INFO": "2014.",
+            "MANAGE_CODE": "MB",
+            "MANAGE_DESC": "도봉어린이문화정보도서관",
+            "SUBJECT_CODE": "3",
+            "SUBJECT_DESC": "사회과학",
+            "USE_LIMIT_CODE": "GM",
+            "USE_LIMIT_DESC": "일반",
+            "ISBN": "9788997969012",
+            "REG_NO": "AR0000012277",
+            "SHELF_LOC_CODE": "001",
+            "SHELF_LOC_DESC": "일반자료실",
+            "SEPARATE_SHELF_CODE": "",
+            "SEPARATE_SHELF_DESC": "적용안함",
+            "CLASS_NO": "373.4",
+            "BOOK_CODE": "고39ㅎ",
+            "WORKING_STATUS": "BOL112N",
+            "WORKING_STATUS_DESC": "배가자료(소장자료)",
+            "BOOK_APPENDIX_FLAG": "B",
+            "BOOK_APPENDIX_DESC": "책"
+          },
+          {
+            "RNUM": 3.0,
+            "SPECIES_KEY": "72044146",
+            "BOOK_KEY": "72044148",
+            "IDX_TITLE": "가을비빔밥 가을 비빔밥 효이란무엇일까요 효이란 무엇일까요",
+            "IDX_AUTHOR": "매이봄 김윤영 매이봄저",
+            "IDX_PUBLISHER": "어썸월드",
+            "IDX_KEYWORD": "",
+            "TITLE_INFO": "가을 비빔밥 : 효이란 무엇일까요?",
+            "AUTHOR_INFO": "매이 봄 저 ; 김윤영 그림.",
+            "PUBLISHER": "어썸월드",
+            "PUB_YEAR_INFO": "2014.",
+            "MANAGE_CODE": "MB",
+            "MANAGE_DESC": "도봉어린이문화정보도서관",
+            "SUBJECT_CODE": "3",
+            "SUBJECT_DESC": "사회과학",
+            "USE_LIMIT_CODE": "GM",
+            "USE_LIMIT_DESC": "일반",
+            "ISBN": "9791157490349",
+            "REG_NO": "AR0000013300",
+            "SHELF_LOC_CODE": "001",
+            "SHELF_LOC_DESC": "일반자료실",
+            "SEPARATE_SHELF_CODE": "유",
+            "SEPARATE_SHELF_DESC": "유",
+            "CLASS_NO": "375.1",
+            "BOOK_CODE": "매68ㄱ",
+            "WORKING_STATUS": "BOL112N",
+            "WORKING_STATUS_DESC": "배가자료(소장자료)",
+            "BOOK_APPENDIX_FLAG": "B",
+            "BOOK_APPENDIX_DESC": "책"
+          },
+          {
+            "RNUM": 4.0,
+            "SPECIES_KEY": "72044150",
+            "BOOK_KEY": "72044152",
+            "IDX_TITLE": "피부자외선 피부 자외선",
+            "IDX_AUTHOR": "정세규 김현정 박하나 장경훈 안성구 박장서 천승현 전병언 홍승필 안성구저",
+            "IDX_PUBLISHER": "정우의학서적",
+            "IDX_KEYWORD": "자외선 피부관리",
+            "TITLE_INFO": "피부 자외선",
+            "AUTHOR_INFO": "안성구 [외]저.",
+            "PUBLISHER": "정우의학서적",
+            "PUB_YEAR_INFO": "2013.",
+            "MANAGE_CODE": "MB",
+            "MANAGE_DESC": "도봉어린이문화정보도서관",
+            "SUBJECT_CODE": "5",
+            "SUBJECT_DESC": "기술과학",
+            "USE_LIMIT_CODE": "GM",
+            "USE_LIMIT_DESC": "일반",
+            "ISBN": "9788995648377",
+            "REG_NO": "AR0000009982",
+            "SHELF_LOC_CODE": "001",
+            "SHELF_LOC_DESC": "일반자료실",
+            "SEPARATE_SHELF_CODE": "",
+            "SEPARATE_SHELF_DESC": "적용안함",
+            "CLASS_NO": "517.45",
+            "BOOK_CODE": "피46ㅈ",
+            "WORKING_STATUS": "BOL112N",
+            "WORKING_STATUS_DESC": "배가자료(소장자료)",
+            "BOOK_APPENDIX_FLAG": "B",
+            "BOOK_APPENDIX_DESC": "책"
+          },
+          {
+            "RNUM": 5.0,
+            "SPECIES_KEY": "72044154",
+            "BOOK_KEY": "72044156",
+            "IDX_TITLE": "자연모방 HARNESSED",
+            "IDX_AUTHOR": "챈기지마크 노승영 CHANGIZIMARK 마크챈기지지음",
+            "IDX_PUBLISHER": "에이도스출판사",
+            "IDX_KEYWORD": "인간진화",
+            "TITLE_INFO": "자연모방",
+            "AUTHOR_INFO": "마크 챈기지 지음 ; 노승영 옮김.",
+            "PUBLISHER": "에이도스출판사",
+            "PUB_YEAR_INFO": "2014.",
+            "MANAGE_CODE": "MB",
+            "MANAGE_DESC": "도봉어린이문화정보도서관",
+            "SUBJECT_CODE": "4",
+            "SUBJECT_DESC": "자연과학",
+            "USE_LIMIT_CODE": "GM",
+            "USE_LIMIT_DESC": "일반",
+            "ISBN": "9788996602286",
+            "REG_NO": "AR0000009973",
+            "SHELF_LOC_CODE": "001",
+            "SHELF_LOC_DESC": "일반자료실",
+            "SEPARATE_SHELF_CODE": "",
+            "SEPARATE_SHELF_DESC": "적용안함",
+            "CLASS_NO": "471.2",
+            "BOOK_CODE": "챈18ㅈ",
+            "WORKING_STATUS": "BOL112N",
+            "WORKING_STATUS_DESC": "배가자료(소장자료)",
+            "BOOK_APPENDIX_FLAG": "B",
+            "BOOK_APPENDIX_DESC": "책"
+          },
+          {
+            "RNUM": 6.0,
+            "SPECIES_KEY": "72044158",
+            "BOOK_KEY": "72044160",
+            "IDX_TITLE": "장제스와국민당엘리티스트 장제스와 국민당엘리티스트 국민당 엘리티스트 1930년대람의사 1930년대 람의사 장개석장제스와국민당엘리티스트 ELITISTFASCISM",
+            "IDX_AUTHOR": "정두음 정두음지음",
+            "IDX_PUBLISHER": "선인",
+            "IDX_KEYWORD": "파시즘 중국정치사상",
+            "TITLE_INFO": "장제스와 국민당 엘리티스트 : 1930년대 藍衣社",
+            "AUTHOR_INFO": "鄭斗音 지음.",
+            "PUBLISHER": "선인",
+            "PUB_YEAR_INFO": "2013.",
+            "MANAGE_CODE": "MB",
+            "MANAGE_DESC": "도봉어린이문화정보도서관",
+            "SUBJECT_CODE": "3",
+            "SUBJECT_DESC": "사회과학",
+            "USE_LIMIT_CODE": "GM",
+            "USE_LIMIT_DESC": "일반",
+            "ISBN": "9788959336425",
+            "REG_NO": "AR0000009988",
+            "SHELF_LOC_CODE": "001",
+            "SHELF_LOC_DESC": "일반자료실",
+            "SEPARATE_SHELF_CODE": "",
+            "SEPARATE_SHELF_DESC": "적용안함",
+            "CLASS_NO": "340.27",
+            "BOOK_CODE": "정26ㅈ",
+            "WORKING_STATUS": "BOL112N",
+            "WORKING_STATUS_DESC": "배가자료(소장자료)",
+            "BOOK_APPENDIX_FLAG": "B",
+            "BOOK_APPENDIX_DESC": "책"
+          },
+          {
+            "RNUM": 7.0,
+            "SPECIES_KEY": "72044162",
+            "BOOK_KEY": "72044164",
+            "IDX_TITLE": "조선중화주의의성립과동아시아 조선중화주의의 성립과동아시아 성립과 동아시아 동아시아한국학연구총서 동아시아한국학 연구총서",
+            "IDX_AUTHOR": "우경섭 우경섭지음",
+            "IDX_PUBLISHER": "문예원",
+            "IDX_KEYWORD": "조선사 한국사",
+            "TITLE_INFO": "조선중화주의의 성립과 동아시아",
+            "AUTHOR_INFO": "우경섭 지음.",
+            "PUBLISHER": "문예원",
+            "PUB_YEAR_INFO": "2013.",
+            "MANAGE_CODE": "MB",
+            "MANAGE_DESC": "도봉어린이문화정보도서관",
+            "SUBJECT_CODE": "9",
+            "SUBJECT_DESC": "역사",
+            "USE_LIMIT_CODE": "GM",
+            "USE_LIMIT_DESC": "일반",
+            "ISBN": "9788997916245",
+            "REG_NO": "AR0000009985",
+            "SHELF_LOC_CODE": "001",
+            "SHELF_LOC_DESC": "일반자료실",
+            "SEPARATE_SHELF_CODE": "",
+            "SEPARATE_SHELF_DESC": "적용안함",
+            "CLASS_NO": "911.05",
+            "BOOK_CODE": "우14ㅈ",
+            "WORKING_STATUS": "BOL112N",
+            "WORKING_STATUS_DESC": "배가자료(소장자료)",
+            "BOOK_APPENDIX_FLAG": "B",
+            "BOOK_APPENDIX_DESC": "책"
+          },
+          {
+            "RNUM": 8.0,
+            "SPECIES_KEY": "72044166",
+            "BOOK_KEY": "72044168",
+            "IDX_TITLE": "이데올로기 문화해부학또는하이퍼코드의문제제기 문화 해부학또는하이퍼코드의문제제기 해부학 또는하이퍼코드의문제제기 또는 하이퍼코드의문제제기 하이퍼코드의 문제제기 문제 제기",
+            "IDX_AUTHOR": "김광현 김광현지음",
+            "IDX_PUBLISHER": "열린책들",
+            "IDX_KEYWORD": "이데올로기 문화",
+            "TITLE_INFO": "이데올로기 : 문화 해부학 또는 하이퍼코드의 문제 제기",
+            "AUTHOR_INFO": "김광현 지음.",
+            "PUBLISHER": "열린책들",
+            "PUB_YEAR_INFO": "2014.",
+            "MANAGE_CODE": "MB",
+            "MANAGE_DESC": "도봉어린이문화정보도서관",
+            "SUBJECT_CODE": "3",
+            "SUBJECT_DESC": "사회과학",
+            "USE_LIMIT_CODE": "GM",
+            "USE_LIMIT_DESC": "일반",
+            "ISBN": "9788932916248",
+            "REG_NO": "AR0000009898",
+            "SHELF_LOC_CODE": "001",
+            "SHELF_LOC_DESC": "일반자료실",
+            "SEPARATE_SHELF_CODE": "",
+            "SEPARATE_SHELF_DESC": "적용안함",
+            "CLASS_NO": "331.5",
+            "BOOK_CODE": "김15ㅇ",
+            "WORKING_STATUS": "BOL112N",
+            "WORKING_STATUS_DESC": "배가자료(소장자료)",
+            "BOOK_APPENDIX_FLAG": "B",
+            "BOOK_APPENDIX_DESC": "책"
+          },
+          {
+            "RNUM": 9.0,
+            "SPECIES_KEY": "72044170",
+            "BOOK_KEY": "72044172",
+            "IDX_TITLE": "독도에관한국제법적쟁점연구 독도에 관한국제법적쟁점연구 관한 국제법적쟁점연구 국제법적 쟁점연구 쟁점 연구 영남대학교독도연구소독도연구총서 영남대학교 독도연구소독도연구총서 독도연구소 독도연구총서",
+            "IDX_AUTHOR": "정갑용 정갑용지음",
+            "IDX_PUBLISHER": "경인문화사",
+            "IDX_KEYWORD": "국제법 독도",
+            "TITLE_INFO": "독도에 관한 국제법적 쟁점 연구",
+            "AUTHOR_INFO": "정갑용 지음.",
+            "PUBLISHER": "景仁文化社",
+            "PUB_YEAR_INFO": "2013.",
+            "MANAGE_CODE": "MB",
+            "MANAGE_DESC": "도봉어린이문화정보도서관",
+            "SUBJECT_CODE": "3",
+            "SUBJECT_DESC": "사회과학",
+            "USE_LIMIT_CODE": "GM",
+            "USE_LIMIT_DESC": "일반",
+            "ISBN": "9788949909523",
+            "REG_NO": "AR0000009889",
+            "SHELF_LOC_CODE": "001",
+            "SHELF_LOC_DESC": "일반자료실",
+            "SEPARATE_SHELF_CODE": "",
+            "SEPARATE_SHELF_DESC": "적용안함",
+            "CLASS_NO": "361.25",
+            "BOOK_CODE": "정12ㄷ",
+            "WORKING_STATUS": "BOL112N",
+            "WORKING_STATUS_DESC": "배가자료(소장자료)",
+            "BOOK_APPENDIX_FLAG": "B",
+            "BOOK_APPENDIX_DESC": "책"
+          },
+          {
+            "RNUM": 10.0,
+            "SPECIES_KEY": "72044174",
+            "BOOK_KEY": "72044176",
+            "IDX_TITLE": "조선서원을움직인사람들 조선 서원을움직인사람들 서원을 움직인사람들 움직인 사람들 국학자료심층연구총서 국학자료 심층연구총서 심층연구 총서",
+            "IDX_AUTHOR": "김명자 박현순 한재훈 정시열 김형수 우인수 한국국학진흥원연구부 정시열지음",
+            "IDX_PUBLISHER": "글항아리",
+            "IDX_KEYWORD": "조선시대 도산서원",
+            "TITLE_INFO": "조선 서원을 움직인 사람들",
+            "AUTHOR_INFO": "정시열 [외]지음 ; 한국국학진흥원 연구부 기획.",
+            "PUBLISHER": "글항아리",
+            "PUB_YEAR_INFO": "2014.",
+            "MANAGE_CODE": "MB",
+            "MANAGE_DESC": "도봉어린이문화정보도서관",
+            "SUBJECT_CODE": "9",
+            "SUBJECT_DESC": "역사",
+            "USE_LIMIT_CODE": "GM",
+            "USE_LIMIT_DESC": "일반",
+            "ISBN": "9788967350918",
+            "REG_NO": "AR0000009918",
+            "SHELF_LOC_CODE": "001",
+            "SHELF_LOC_DESC": "일반자료실",
+            "SEPARATE_SHELF_CODE": "",
+            "SEPARATE_SHELF_DESC": "적용안함",
+            "CLASS_NO": "911.05",
+            "BOOK_CODE": "조54ㄱ",
+            "WORKING_STATUS": "BOL112N",
+            "WORKING_STATUS_DESC": "배가자료(소장자료)",
+            "BOOK_APPENDIX_FLAG": "B",
+            "BOOK_APPENDIX_DESC": "책"
+          }
+        ]
+      },
+      "numOfRows": 10,
+      "pageNo": 1,
+      "totalCount": 123570
+    }
+  }
+}`;
+
+let result = [];
+function formatJSON(json, paths) {
+  json = JSON.parse(json);
+  if (!(paths instanceof Array)) {
+    paths = [paths];
+  }
+  paths.forEach((path) => {
+    path = path.split(".");
+    path.shift();
+    resultOnlyOne = formatJSONOnlyOne(json, path);
+    result.push(resultOnlyOne);
+  });
+  if (result.length === 1) {
+    result = result[0];
+  }
+}
+
+function formatJSONOnlyOne(data, path) {
+  key = path.shift();
+  if (key === undefined) return data;
+  try {
+    if (data[key] === undefined) {
+      return path.length ? `Invalid Key : ${key}` : undefined;
+    }
+    if (data[key] instanceof Array) {
+      const valArray = [];
+      for (item of data[key]) {
+        valArray.push(formatJSONOnlyOne(item, [...path]));
+      }
+      return valArray;
+    }
+    return formatJSONOnlyOne(data[key], path);
+  } catch (error) {
+    return `Unknown Error`;
+  }
+}
+
+let paths = ["api.response.body.items.item.BOOK_KEY"];
+formatJSON(json, paths);
+
+console.log(result);
