1e5259a2111043ed5153a93527f48bf6b5f2bff7
김제관 <jekwan@kakao.com>
2022-09-19 17:59:46 +0900

    [add] test socket code

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -3,7 +3,7 @@ let initialized = false;
 module.exports = function (RED) {
   if (!initialized) {
     initialized = true;
-    init(RED.httpNode || RED.httpAdmin, RED.settings);
+    init(RED);
   }
 
   return {
@@ -12,14 +12,38 @@ module.exports = function (RED) {
 };
 
 const path = require("path");
+const socketio = require("socket.io");
 const serveStatic = require("serve-static");
 
-function init(app, settings) {
+let io = null;
+
+function init(RED) {
+  const app = RED.httpNode || RED.httpAdmin;
+  const server = RED.server;
+
+  io = socketio(server);
+  initSocket(io);
+
+  // const settings = RED.settings;
   // const path = `${settings.httpNodeRoot}${settings.path}`;
   // app.use(path, serveStatic(path.join(__dirname, "/dist")));
 
   app.use("/dashboard", serveStatic(path.join(__dirname, "/dist")));
   console.log("react dashboard started at /dashboard");
+}
+
+function initSocket(io) {
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+
+    socket.emit("hello", "world");
+    socket.emit("hello", "world");
+    socket.emit("hello", "world");
+    socket.emit("hello", "world");
+  });
 }
 
 function testFunc() {
--- dashboard/src/components/app.js
+++ dashboard/src/components/app.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import io from "socket.io-client";
 
-export default function App() {
-  return <div>Hello Dashboard App</div>;
-}
+const socket = io.connect("http://localhost:1880");
+
+const App = () => {
+  useEffect(() => {
+    socket.on("connection", ({ name, message }) => {
+      console.log(name, message);
+    });
+
+    socket.on("hello", (msg) => {
+      console.log(msg);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <>
+      <div>Hello Dashboard</div>
+    </>
+  );
+};
+
+export default App;
