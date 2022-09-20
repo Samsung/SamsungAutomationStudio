865c596b8b3ae938d8e005ffe80bc8025d50bb70
jekwan <jekwan@kakao.com>
2022-09-20 12:36:21 +0900

    [modify] apply css/style loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: [
