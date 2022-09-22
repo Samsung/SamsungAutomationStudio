6e67c51270235993e5a15fc45f127be8b808872e
DESKTOP-KVCQHCD\SSAFY <duddnd904@gmail.com>
2022-09-22 20:48:00 +0900

    chore : desktop - labtop synch

--- object_detection/object-detection.js
+++ object_detection/object-detection.js
@@ -1,19 +1,24 @@
 module.exports = function(RED){
 
     function ObjectDetectionNode(config){
-        RED.nodes.createNode(this, config)
-            var node = this;
-            
+
+        RED.nodes.createNode(this, config);
+
+        this.on('input', function(msg, send){
+            console.log("module console " + msg.payload.dbSavedData);
+            config.dbSavedData = msg.payload.dbSavedData;
+            msg.payload = HTML()
+            send = send || function(){this.send.apply(this, arguments)}
+            send(msg)
+        })
+
+       
+
         function HTML() {
             return require('./object-detection-html.js').code(config)
         }
-
-            node.on('input', function(msg, send){
-                msg.payload = HTML()
-                send = send || function(){this.send.apply(this, arguments)}
-                send(msg)
-            })
     }
-
     RED.nodes.registerType("object-detection", ObjectDetectionNode);
+
+
 }
