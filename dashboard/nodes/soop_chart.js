785e98f930e11dcc925ba728fc50068501c055c8
heewon7318 <huiwon1488@gmail.com>
2022-09-28 14:07:22 +0900

    [Modify] add x,y value / prettier formatting

--- dashboard/nodes/soop_chart.js
+++ dashboard/nodes/soop_chart.js
@@ -1,68 +1,80 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
   const dashboard = require("../dashboard")(RED);
 
   function SoopChartNode(config) {
     const node = this;
     RED.nodes.createNode(node, config);
-    
+
     node.on("input", function (msg, send, done) {
-      if (isNaN(msg.payload)) { return; }
+      if (isNaN(msg.payload)) {
+        return;
+      }
 
       const group = RED.nodes.getNode(config.group);
-      if(!group) { return; }
+      if (!group) {
+        return;
+      }
       var tab = RED.nodes.getNode(group.config.tab);
-      if (!tab) { return; }
+      if (!tab) {
+        return;
+      }
       const chartType = config.chartType;
       let extra = {};
       let state = {
         nodeId: node.id,
-        nodeType: 'chart',
+        nodeType: "chart",
         group: group,
         size: [
-          parseInt(config.height || group.config.width/2+1 || 4), 
-          parseInt(config.width || group.config.width || 6)
+          parseInt(config.height || group.config.width / 2 + 1 || 4),
+          parseInt(config.width || group.config.width || 6),
+          parseInt(config.widgetX),
+          parseInt(config.widgetY),
         ],
         title: config.label,
         chartType: config.chartType,
-        data: { label: msg.topic ? msg.topic:"dataset", value:msg.payload },
+        data: { label: msg.topic ? msg.topic : "dataset", value: msg.payload },
         legend: config.legend,
         blankLabel: config.blankLabel,
       };
-      
-      if (chartType === 'line') {
+
+      if (chartType === "line") {
         extra = {
-          RmMethod: config.removePastDataPoints ? 'point' : 'time',
+          RmMethod: config.removePastDataPoints ? "point" : "time",
           RmTime: config.removePastData * config.removePastDataUnit,
           RmPoint: config.removePastDataPoints,
           xAxisFormat: config.xAxisFormat,
           yMin: config.yMin,
           yMax: config.yMax,
           interpolate: config.interpolate,
-          customValue: config.xAxisFormat === 'custom' ? config.customValue : ''
-        } 
-      } else if (chartType === 'bar'){
+          customValue: config.xAxisFormat === "custom" ? config.customValue : "",
+        };
+      } else if (chartType === "bar") {
         extra = {
           yMin: config.yMin,
           yMax: config.yMax,
-        }
-      } else if (chartType === 'horizontalBar'){
+        };
+      } else if (chartType === "horizontalBar") {
         extra = {
           xMin: config.xMin,
           xMax: config.xMax,
-        }
+        };
       } else {
         extra = {
           cutout: parseInt(config.cutout || 0),
-        }
+        };
       }
-      state = Object.assign(state, extra)
-      
+      state = Object.assign(state, extra);
+
       // send state to dashboard
-      dashboard.emitState(state)
-      
+      dashboard.emitState(state);
+
       // send message
-      send = send || function() { node.send.apply(node,arguments) }
-      msg.payload = state
+      send =
+        send ||
+        function () {
+          node.send.apply(node, arguments);
+        };
+      msg.payload = state;
       send(msg);
 
       // done
@@ -76,7 +88,7 @@ module.exports = function(RED) {
         node.log("This node is being restarted");
       }
       done();
-    });   
+    });
   }
-  RED.nodes.registerType("soop_chart",SoopChartNode);
-}
+  RED.nodes.registerType("soop_chart", SoopChartNode);
+};
