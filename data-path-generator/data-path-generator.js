d5c99a6640101f207023876e6ff3be86ae504846
박승훈 <tmdgns1126@naver.com>
2022-09-28 10:40:02 +0900

    remove: the data path generator directory

--- data-path-generator/data-path-generator.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (RED) {
-  function DataPathGenerator(config) {
-    RED.nodes.createNode(this, config);
-  }
-  RED.nodes.registerType("data-path-generator", DataPathGenerator);
-};
