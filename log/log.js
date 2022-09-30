aa81450fad2d5db64f44791e9fe71c0539497751
홍주성 <hjs8482@naver.com>
2022-09-30 16:14:57 +0900

    hotfix: fix messages form in determine node and log node

--- log/log.js
+++ log/log.js
@@ -8,13 +8,24 @@ module.exports = function (RED) {
         const fs = require('fs')
 
         node.on('input', function (msg, send) {
-            let fullPath = `${config.logDirectory}` + "/log" + msg.payload.date.substring(0,10)
+
+            let today = new Date();
+
+            let date = today.toLocaleString()
+            msg.payload.Date = date
+            let directory = `${config.logDirectory}`
+            const isExists = fs.existsSync(directory)
+            if (!isExists) {
+                fs.mkdirSync(directory, { recursive: true })
+            }
             
-            fs.appendFile(fullPath, JSON.stringify(msg.payload)+"\r\n", function (err) {
+            let fullPath = `${config.logDirectory}` + "/log-" + today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+
+            fs.appendFile(fullPath, JSON.stringify(msg.payload) + "\r\n", function (err) {
                 if (err) throw err
             })
 
-            send(msg)
+            return
         })
     }
 
---
+++ object-person-determine/readme.md
@@ -0,0 +1,17 @@
+## Object-Person-Determine
+---
+A node that determines which of the detected objects most interacts with the user.
+
+
+### input message properties
+```
+msg.payload = {
+    log : {
+        data : [],
+        date : ''
+    },
+
+}
+
+
+```
