f13acb2eff98fe99e9dbef1b38bd2d4838d53225
김혜림 <bljh1008@naver.com>
2022-10-01 01:18:13 +0900

    [modify] dropdown component

--- dashboard/src/components/SoopDropdown.js
+++ dashboard/src/components/SoopDropdown.js
@@ -1,47 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import { sendMessage } from "../utils/socket";
 
+// TODO: exampleData -> props
+// TODO: x, y, w, h에서 받아오면 계산하기
+// TODO: top, left 옵션줘서 위치 배정하기
 const DropdownContainer = styled.div`
-  width: 200px;
+  position: absolute;
+  top: 30px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 5px 10px;
+  box-sizing: border-box;
 `;
 
 const SoopDropdown = () => {
-  const [option, setOption] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+  const [currentOptions, setCurrentOptions] = useState([]);
+  const [currentLabel, setCurrentLabel] = useState("");
 
   const exampleData = {
-    label: "dropdown",
-    tooltip: "dropdown node",
-    placeholder: "Select option",
-    option: { option1: "value1", option2: "value2" },
+    node: {
+      label: "dropdown라벨",
+      tooltip: "dropdown node",
+      placeholder: "Select option",
+      option: { option1: "value1", option2: "value2" },
+    },
+    states: {},
   };
-  const entries = Object.entries(exampleData.option);
 
-  const onChange = (e) => {
-    // FIXME: 현재 value로만 변경값이 저장되는데.. key는 어떻게 해야할지 허허
-    console.log(e.target.value);
-    setOption(e.target.value);
+  const onChange = e => {
+    setSelectedOption(e.target.value);
+    // TODO: sendMessage 활성화하기
+    // sendMessage(node.id, { value: option });
   };
+
+  useEffect(() => {
+    const optionsArray = Object.entries(exampleData.node.option);
+    setCurrentOptions(optionsArray);
+    setCurrentLabel(exampleData.node.label);
+  }, []);
+
   return (
     <>
       <DropdownContainer>
         <FormControl fullWidth>
-          <InputLabel id="soop-dashboard-select-label">
-            {exampleData.label}
-          </InputLabel>
-          <Select
-            labelId="soop-dashboard-select-label"
-            label={exampleData.placeholder}
-            value={option}
-            onChange={onChange}
-          >
-            {entries.map((entry, idx) => {
+          <InputLabel id="soop-dashboard-select-label">{currentLabel}</InputLabel>
+          <Select labelId="soop-dashboard-select-label" label={currentLabel} value={selectedOption} onChange={onChange}>
+            {currentOptions.map((cOption, idx) => {
               return (
-                <MenuItem key={idx} value={entry[1]}>
-                  {entry[1]}
+                <MenuItem key={idx} value={cOption[0]}>
+                  {cOption[1]}
                 </MenuItem>
               );
             })}
