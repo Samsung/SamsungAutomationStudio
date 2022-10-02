160d7ccb46e1166a247a37d26804a1dba500188f
quaternion12345 <69517473+quaternion12345@users.noreply.github.com>
2022-10-02 23:39:48 +0900

    [modify] slider and image node communication (#54)
    Co-authored-by: fermion <fermion>

--- dashboard/nodes/soop_slider.js
+++ dashboard/nodes/soop_slider.js
@@ -7,15 +7,6 @@ module.exports = function (RED) {
 
     var node = this;
 
-    // var group = RED.nodes.getNode(config.group);
-    // if (!group) {
-    //   return;
-    // }
-    // var tab = RED.nodes.getNode(group.config.tab);
-    // if (!tab) {
-    //   return;
-    // }
-
     // Add node information & event listener from dashboard
     dashboard.addNode({
       node: node,
@@ -25,31 +16,15 @@ module.exports = function (RED) {
         });
       },
     });
-    
-    // Send runtime information to dashboard
-    var step = config.step > Math.abs(config.min - config.max) ? 1 : config.step;
-    var state = {
-      nodeId: node.id,
-      nodeType: config.type,
-      group: config.group, // need to revise
-      size: [], // need to revise
-      name: config.name,
-      color: config.colorPicking,
-      label: config.label,
-      tooltip: config.tooltip,
-      range: [config.min, config.max, step],
-      when: config.send,
-      invert: parseFloat(config.min) > parseFloat(config.max) ? true : false,
-      payload: Math.min(config.min, config.max),
-    }
-
-    dashboard.emitState(state);
 
     // Receive msg from upstream node in a flow
     node.on("input", function (msg) {
-      if(config.pass){
-        state.payload = Math.max(config.min, Math.min(config.max, parseInt(msg.payload)));
-        dashboard.emitState(state);
+      if(config.pass && parseInt(msg.payload) != NaN){
+        state.payload = 
+        dashboard.emitState({
+          nodeId: node.id,
+          value: Math.max(config.min, Math.min(config.max, parseInt(msg.payload)))
+        })
       }
     });
   }
