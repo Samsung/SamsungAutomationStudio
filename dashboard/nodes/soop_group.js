e9b4d142f990a6a8436b6f6593d0d6d4198f267f
jekwan <jekwan@kakao.com>
2022-10-03 00:28:26 +0900

    [modify] fix wrong bracket

--- dashboard/nodes/soop_group.js
+++ dashboard/nodes/soop_group.js
@@ -1,28 +1,27 @@
-module.exports = function(RED) {
-    function GroupNode(config) {
-        const node = this;
-        RED.nodes.createNode(this, config);
-        this.config = {
-            name: config.name,
-            displayVisible: config.displayVisible,
-            groupX: config.groupX,
-            groupY: config.groupY,
-            width: config.width,
-            height: config.height,
-            order: config.order,
-            tab: config.tab,
-            groupState: config.groupState,
-            includedNodesId: config.includedNodesId,
-        };
-        if (!this.config.hasOwnProperty("displayVisible")) {
-            this.config.displayVisible = true;
-        }
-        if (this.config.displayVisible !== false) {
-            this.config.displayVisible = true;
-        }
-        for (var i = 0; i < this.config.height; i++) {
-            this.config.groupState[i] = new Array(this.config.width);
-        }
+module.exports = function (RED) {
+  function GroupNode(config) {
+    const node = this;
+    RED.nodes.createNode(this, config);
+    this.config = {
+      name: config.name,
+      displayVisible: config.displayVisible,
+      groupX: config.groupX,
+      groupY: config.groupY,
+      width: config.width,
+      height: config.height,
+      order: config.order,
+      tab: config.tab,
+      groupState: config.groupState,
+      includedNodesId: config.includedNodesId,
+    };
+    if (!this.config.hasOwnProperty("displayVisible")) {
+      this.config.displayVisible = true;
+    }
+    if (this.config.displayVisible !== false) {
+      this.config.displayVisible = true;
+    }
+    for (var i = 0; i < this.config.height; i++) {
+      this.config.groupState[i] = new Array(this.config.width);
     }
   }
 
