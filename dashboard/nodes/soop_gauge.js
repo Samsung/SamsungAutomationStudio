431d92e97f4c81b4955286f266bedf159706df15
zzunsik <71685888+zzunsik@users.noreply.github.com>
2022-10-02 23:50:45 +0900

    Modify attribute, state, liquid range (#51)
    * [modify] send msg, attribute, help text
    
    * [modify] grid, size attribute
    
    * [modify] node color, grid, size attribute
    
    * [modify] node color
    
    * [modify] attribute, state, liquid range
    
    * [modify] delete log code

--- dashboard/nodes/soop_gauge.js
+++ dashboard/nodes/soop_gauge.js
@@ -5,32 +5,6 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     const node = this;
 
-    // const group = RED.nodes.getNode(config.group);
-    // if (!group) {
-    //   return;
-    // }
-    // const tab = RED.nodes.getNode(group.config.tab);
-    // if (!tab) {
-    //   return;
-    // }
-
-    const group = "";
-    let state = {
-      nodeId: node.id,
-      nodeType: config.type,
-      group: config.group,
-      size: [parseInt(config.width), parseInt(config.height), parseInt(config.widgetX), parseInt(config.widgetY)],
-      name: config.name,
-      time: "",
-      gType: config.gType,
-      label: config.label,
-      format: config.format,
-      range: [config.min, config.max],
-      value: config.min,
-      units: config.units,
-      color: config.colorPicking,
-    };
-
     node.on("input", function (msg) {
       let form = config.format.replace(/{{/g, "").replace(/}}/g, "").replace(/\s/g, "") || "_zzz_zzz_zzz_";
 
@@ -38,16 +12,18 @@ module.exports = function (RED) {
       form = form.split(".")[1];
       let value = RED.util.getMessageProperty(msg, form);
       if (value !== undefined) {
-        if (!isNaN(parseFloat(value))) {
-          value = parseFloat(value);
+        if (!isNaN(parseInt(value))) {
+          value = parseInt(value);
+        } else {
+          value = config.min;
         }
       } else {
-        if (!isNaN(parseFloat(msg.payload))) {
-          value = parseFloat(msg.payload);
-        }
+        value = config.min;
       }
-      state.value = value;
-      dashboard.emitState(state);
+      dashboard.emitState({
+        nodeId: node.id,
+        value: value,
+      });
     });
   }
   RED.nodes.registerType("soop_gauge", SoopGaugeNode);
