1191eff07843f976ba5cc0bcc271f03239470f20
전상현 <adardua@gmail.com>
2022-10-02 17:11:41 +0900

    [modify]tab and group label change

--- dashboard/nodes/soop_group.js
+++ dashboard/nodes/soop_group.js
@@ -1,24 +1,33 @@
 module.exports = function(RED) {
+    const dashboard = require("../dashboard")(RED);
 
     function GroupNode(config) {
+        const node = this;
         RED.nodes.createNode(this, config);
         this.config = {
             name: config.name,
             displayVisible: config.displayVisible,
-            groupX: config.x,
-            groupY: config.y,
+            groupX: config.groupX,
+            groupY: config.groupY,
             width: config.width,
             height: config.height,
             order: config.order,
             tab: config.tab,
             groupState: config.groupState,
-            includedNodesId: config.includedNodesId
+            includedNodesId: config.includedNodesId,
         };
-        if (!this.config.hasOwnProperty("displayVisible")) { this.config.displayVisible = true; }
-        if (this.config.displayVisible !== false) { this.config.displayVisible = true; }
+        if (!this.config.hasOwnProperty("displayVisible")) {
+            this.config.displayVisible = true;
+        }
+        if (this.config.displayVisible !== false) {
+            this.config.displayVisible = true;
+        }
         for (var i = 0; i < this.config.height; i++) {
             this.config.groupState[i] = new Array(this.config.width);
         }
+        dashboard.addNode({
+            node: node,
+        });
     }
 
     RED.nodes.registerType("soop_group", GroupNode);
