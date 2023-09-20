75b4234e979983b5dd5092a3fdcdc19aed87f7f3
chickennight <john0513@naver.com>
2023-09-20 10:43:49 +0900

    feat: self image translater exists codes

--- GOOD-I-DEER/nodes/face-vectorization-node/face-vectorization.js
+++ GOOD-I-DEER/nodes/face-vectorization-node/face-vectorization.js
@@ -1,41 +1,66 @@
 module.exports = function (RED) {
+  const onnx = require("onnxruntime-node");
+  const sharp = require("sharp");
+
   function FaceVectorizationNode(config) {
     RED.nodes.createNode(this, config);
+    const node = this;
 
-    this.on("input", async function (msg) {
-      const tf = require("@tensorflow/tfjs-node");
-      const { createCanvas, loadImage } = require("canvas");
+    // FaceNet ONNX 모델 파일 경로 설정
+    const modelPath = `${__dirname}/model/facenet-model.onnx`; // FaceNet ONNX 모델 파일 경로
 
-      async function loadFaceNetModel(modelPath) {
-        const model = await tf.loadLayersModel(`file://${modelPath}`);
-        return model;
-      }
+    node.on("input", async function (msg) {
+      // 입력 이미지 추출 (예: 이미지를 픽셀 배열로 변환)
+      const inputData = msg.payload; // 이미지 데이터 혹은 이미지 피처 추출
 
-      async function getFaceEmbedding(model, imagePath) {
-        const img = await loadImage(imagePath);
-        const canvas = createCanvas(160, 160);
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, 160, 160);
+      // ONNX 모델 로드
+      const model = await onnx.InferenceSession.create(modelPath);
 
-        const tensor = tf.node.encodePng(canvas);
-        const normalizedTensor = tf.div(tensor, 255.0);
-        const batchedTensor = normalizedTensor.expandDims(0);
+      // 입력 데이터를 Tensor로 변환
+      const inputName = model.inputNames[0]; // 모델의 첫 번째 입력 이름을 가져옵니다.
 
-        const predictions = await model.predict(batchedTensor);
-        const embedding = predictions.dataSync();
+      // 입력 데이터의 차원을 설정 (예시: 이미지 차원)
+      const inputDims = [1, 3, 224, 224]; // [배치 크기, 채널 수, 높이, 너비]
+      const input = await prepare_input(inputData);
+      const inputTensor = new onnx.Tensor(Float32Array.from(input), inputDims);
 
-        return embedding;
-      }
+      // 입력 데이터를 feeds 객체에 설정
+      const feeds = {};
+      feeds[inputName] = inputTensor;
 
-      const modelPath = "./model/20180402-114759.pb";
-      const imagePath = msg.payload;
+      // ONNX 모델 실행
+      const outputData = await model.run(feeds);
 
-      const model = await loadFaceNetModel(modelPath);
-      const embedding = await getFaceEmbedding(model, imagePath);
+      // 결과를 출력 메시지에 설정
+      msg.payload = outputData;
 
-      msg.payload = embedding;
-      this.send(msg);
+      // 결과 메시지 전송
+      node.send(msg);
     });
+
+    async function prepare_input(buf) {
+      const img = sharp(buf);
+      const pixels = await img
+        .removeAlpha()
+        .resize({ width: 224, height: 224, fit: "fill" })
+        .raw()
+        .toBuffer();
+      // print("pixels",pixels)
+      // console.log("pixels.length: " + pixels.length);
+      const red = [],
+        green = [],
+        blue = [];
+      for (let index = 0; index < pixels.length; index += 3) {
+        red.push(pixels[index] / 255.0);
+        green.push(pixels[index + 1] / 255.0);
+        blue.push(pixels[index + 2] / 255.0);
+      }
+      const input = [...red, ...green, ...blue];
+      // print("input", input);
+      // console.log("input.length: " + input.length);
+      console.log(input);
+      return input;
+    }
   }
   RED.nodes.registerType("face-vectorization", FaceVectorizationNode);
 };
