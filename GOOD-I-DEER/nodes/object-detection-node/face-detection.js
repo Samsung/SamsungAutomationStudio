d1bc8c00ea8cf933baeb9263417071c1e97a87e4
이덕용 <leedeok1996@naver.com>
2023-09-21 12:31:49 +0900

    feat : face detection v1

--- GOOD-I-DEER/nodes/object-detection-node/face-detection.js
+++ GOOD-I-DEER/nodes/object-detection-node/face-detection.js
@@ -3,21 +3,51 @@ module.exports = function (RED) {
     RED.nodes.createNode(this, config);
     const ort = require("onnxruntime-node");
     const sharp = require("sharp");
+    const fs = require("fs");
+
     const node = this;
+    const returnType = Number(config.returnType);
+    const saveDir = config.absolutePathDir;
+    let bufferFromImage;
 
     node.on("input", async function (msg) {
-      msg.payload = await detect_face_on_image(msg.payload);
-      node.send(msg);
+      const img = sharp(msg.payload);
+      bufferFromImage = msg.payload;
+      if (returnType <= 1) {
+        if (returnType === 0) {
+          msg.payload = await detect_face_on_image_informations(img);
+        } else if (returnType === 1) {
+          msg.payload = await detect_face_on_image_imageBuffer(img);
+        }
+        node.send(msg);
+      } else if (returnType === 2) {
+        // 저장 위치 존재하는지 검사해야함!!!!!!!!!!!!
+        if (fs.existsSync(saveDir)) {
+          await detect_face_on_image_save(img);
+        } else {
+          node.error("folder dosen't exists");
+        }
+      }
     });
 
-    async function detect_face_on_image(buf) {
-      const [input, img_width, img_height] = await prepare_input(buf);
+    async function detect_face_on_image_informations(img) {
+      const [input, img_width, img_height] = await prepare_input(img);
       const output = await run_model(input);
-      return process_output(output, img_width, img_height);
+      const boxes = process_output(output, img_width, img_height);
+      return get_image_informations(boxes);
     }
 
-    async function prepare_input(buf) {
-      const img = sharp(buf);
+    async function detect_face_on_image_imageBuffer(img) {
+      const boxes = await detect_face_on_image_informations(img);
+      return get_image_buffers(boxes);
+    }
+
+    async function detect_face_on_image_save(img) {
+      const boxes = await detect_face_on_image_informations(img);
+      save_images(boxes);
+    }
+
+    async function prepare_input(img) {
       const md = await img.metadata();
 
       const [img_width, img_height] = [md.width, md.height];
@@ -72,18 +102,86 @@ module.exports = function (RED) {
       boxes = boxes.sort((box1, box2) => box2[5] - box1[5]);
       const result = [];
       while (boxes.length > 0) {
-        const info = {
-          label: boxes[0][4],
-          x: boxes[0][0],
-          y: boxes[0][1],
-          w: boxes[0][2] - boxes[0][0],
-          h: boxes[0][3] - boxes[0][1],
-          prob: boxes[0][5],
-        };
-        result.push(info);
+        result.push(boxes[0]);
         boxes = boxes.filter((box) => iou(boxes[0], box) < 0.7);
       }
       return result;
+    }
+
+    function get_image_informations(boxes) {
+      const result = [];
+      boxes.forEach((box) => {
+        const info = {
+          label: box[4],
+          x: box[0],
+          y: box[1],
+          w: box[2] - box[0],
+          h: box[3] - box[1],
+          prob: box[5],
+        };
+        result.push(info);
+      });
+      return result;
+    }
+
+    async function get_image_buffers(boxes) {
+      const result = [];
+      await Promise.all(
+        boxes.map(async (box) => {
+          const buffer = await makeBuffer(box);
+          result.push(buffer);
+        })
+      );
+      return result;
+    }
+
+    function save_images(boxes) {
+      let faceCount = 1;
+      const today = new Date();
+      const dateformat =
+        "/" +
+        today.getFullYear() +
+        (today.getMonth() + 1 < 10
+          ? "0" + (today.getMonth() + 1)
+          : today.getMonth() + 1) +
+        (today.getDate() < 10 ? "0" + today.getDate() : today.getDate()) +
+        (today.getHours() < 10 ? "0" + today.getHours() : today.getHours()) +
+        (today.getMinutes() < 10
+          ? "0" + today.getMinutes()
+          : today.getMinutes()) +
+        (today.getSeconds() < 10
+          ? "0" + today.getSeconds()
+          : today.getSeconds());
+      boxes.forEach(async (box) => {
+        const outputImage =
+          saveDir + dateformat + "_" + "face" + faceCount++ + ".png";
+        sharp(bufferFromImage)
+          .extract({
+            width: parseInt(box.w),
+            height: parseInt(box.h),
+            left: parseInt(box.x),
+            top: parseInt(box.y),
+          })
+          .toFile(outputImage)
+          .then(() => node.trace("Image cropped and saved in saveDir"))
+          .catch(() =>
+            node.error("An error occured, when image cropped and saved")
+          );
+      });
+    }
+
+    async function makeBuffer(box) {
+      const buffer = await sharp(bufferFromImage)
+        .extract({
+          width: parseInt(box.w),
+          height: parseInt(box.h),
+          left: parseInt(box.x),
+          top: parseInt(box.y),
+        })
+        .toFormat("png")
+        .toBuffer();
+
+      return buffer;
     }
 
     function iou(box1, box2) {
