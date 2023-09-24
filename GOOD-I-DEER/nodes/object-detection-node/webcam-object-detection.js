a36b80b48d886ecdda7a1bc3521b3963e057701a
이채림 <leecr1215@naver.com>
2023-09-24 16:17:03 +0900

    refactor : remove console log

--- GOOD-I-DEER/nodes/object-detection-node/webcam-object-detection.js
+++ GOOD-I-DEER/nodes/object-detection-node/webcam-object-detection.js
@@ -8,23 +8,15 @@ module.exports = function(RED) {
         const node = this;
         
         function HTML() {
-            console.log("HTML()의 config: ", config);
             return require('./webcam-html.js').code(config)
         }
         
-          
-        console.log('webcamYoloNode의 config')
-        console.log(config);
-        
-        node.on('input', (msg, send, done) => {
-            console.log('==============msg============');
-            console.log(msg);
-        
+        node.on('input', (msg, send, done) => {        
             msg.payload = HTML()
             send = send || function() { this.send.apply(this, arguments )}
             send(msg)
         })
     }
-    
+
     RED.nodes.registerType("webcam-object-detection", webcamYoloNode)
 }
